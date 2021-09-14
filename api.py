2ccb3d44aad196052953905ed13f0ff38b5888ad
lucifer-999 <ayush.bajaj3210@gmail.com>
2021-09-14 14:37:17 +0530

    Fixed bugs

--- api.py
+++ api.py
@@ -2,7 +2,7 @@ from flask import Flask, jsonify
 from flask_restful import Api, Resource
 import json
 
-app = Flask(_name_)
+app = Flask(__name__)
 api = Api(app)
 
 file = open("data.json")
@@ -26,6 +26,4 @@ class UserAPI (Resource):
 
 api.add_resource(UserAPI, '/users/<int:id>', endpoint = 'user')
 
-
-if _name_ == "_main_":
-	app.run(host='0.0.0.0', port=5001)
+app.run(host='0.0.0.0', port=5001)
